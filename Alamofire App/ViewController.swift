32ec112d4bf4a9072d9a7bc1a78b3fcb42f66cc7
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-02 20:40:48 +0100

    Initial commit

---
+++ Alamofire App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Alamofire App
+//
+//  Created by Macbook on 2. 3. 2021..
+//
+
+import UIKit
+import Alamofire
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
